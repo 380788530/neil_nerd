d91cc8d3574a589b2f06caed5f4a64bd5b563784
380788530 <380788530@qq.com>
2017-08-19 16:25:26 +0800

    sdf

---
+++ src/main/java/com/neil/impl/HelloWorld.java
@@ -0,0 +1,22 @@
+package com.neil.impl;
+
+/**
+ * ******************************************************************
+ * <p><b></b><br/>
+ * <p><b>备注：</b><br/>
+ * ******************************************************************
+ *
+ * @param @return 设定文件
+ * @author lizhiyong
+ * @Title:
+ * @Description: TODO(这里用一句话描述这个方法的作用)
+ * @return String    返回类型
+ * @date 2017-08-16 20:10
+ * @throws
+ */
+public class HelloWorld {
+
+    public void sayHelloWorld() {
+        System.out.println("HelloWorld!");
+    }
+}
---
+++ src/main/java/com/neil/impl/TableDAO.java
@@ -0,0 +1,31 @@
+package com.neil.impl;
+
+/**
+ * ******************************************************************
+ * <p><b></b><br/>
+ * <p><b>备注：</b><br/>
+ * ******************************************************************
+ *
+ * @param @return 设定文件
+ * @author lizhiyong
+ * @Title:
+ * @Description: TODO(这里用一句话描述这个方法的作用)
+ * @return String    返回类型
+ * @date 2017-08-18 14:52
+ * @throws
+ */
+public class TableDAO {
+
+    public void create(){
+        System.out.println("create() is running !");
+    }
+    public void query(){
+        System.out.println("query() is running !");
+    }
+    public void update(){
+        System.out.println("update() is running !");
+    }
+    public void delete(){
+        System.out.println("delete() is running !");
+    }
+}
